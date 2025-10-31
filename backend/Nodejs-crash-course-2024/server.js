5854effd6f39d4f995e75ae305a9ed1804632719
albertombundi <albertombundi@outlook.com>
2025-10-31 12:28:55 +0100

    HTTP Module & Create server

---
+++ backend/Nodejs-crash-course-2024/server.js
@@ -0,0 +1,14 @@
+import http from 'http';
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+    // res.end('<h1>Hello, World!</h1>');
+    // res.statusCode = 404;
+
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Server Error' }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
