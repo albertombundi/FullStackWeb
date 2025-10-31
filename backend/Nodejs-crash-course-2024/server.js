8d20bac2c2b147699cd07fe02c9430a202725a83
albertombundi <albertombundi@outlook.com>
2025-10-31 22:13:54 +0100

    Environments variables & .env

--- backend/Nodejs-crash-course-2024/server.js
+++ backend/Nodejs-crash-course-2024/server.js
@@ -1,9 +1,10 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
     // res.end('<h1>Hello, World!</h1>');
     // res.statusCode = 404;
+    
 
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('<h1>Hello, World!</h1>');
