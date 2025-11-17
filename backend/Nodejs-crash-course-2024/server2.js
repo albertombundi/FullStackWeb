b1ce7b8063c1be1f34fbe5dc28457581af50a49f
albertombundi <albertombundi@outlook.com>
2025-11-17 22:27:46 +0100

    Cleanup(Middleware & Handlers)

--- backend/Nodejs-crash-course-2024/server2.js
+++ backend/Nodejs-crash-course-2024/server2.js
@@ -1,6 +1,8 @@
-import { createServer } from 'http';
+import { createServer, get } from 'http';
+import { json } from 'stream/consumers';
 const PORT = process.env.PORT;
 
+// Dados simulados de usuários
 const users = [
     { id: 1, name: 'John Doe' },
     { id: 2, name: 'Jane Doe' },
@@ -14,34 +16,57 @@ const logger = (req, res, next) => {
 };
 
 
+//Middleware JSON
+const JsonMiddleware = (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+}
+
+// Manipulador de rota para GET api/users
+const getUsersHandler = (req, res) => {
+    res.write(JSON.stringify(users));
+    res.end();
+}
+
+// Manipulador de rota para GET api/users/:id
+const getUserByIdHandler = (req, res) => {
+    const id = req.url.split('/')[3];
+    const user = users.find((user) => user.id === parseInt(id));
+
+    if (user) {
+        res.write(JSON.stringify(user));
+    } else {
+        res.statusCode = 404;
+        res.write(JSON.stringify({ message: 'User not found' }));
+    }
+    res.end();
+};
+
+
+// Not Found Handler
+const notFoundHandler = (req, res) => {
+    res.statusCode = 404;
+    res.write(JSON.stringify({ message: 'Route not found' }));
+    res.end();
+};
+
 // Servidor HTTP simples com rotas para obter usuários
 const server = createServer((req, res) => {
     logger(req, res, () => {
-        if (req.url === '/api/users' && req.method === 'GET') {
-            res.setHeader('Content-Type', 'application/json');
-            res.write(JSON.stringify(users));
-            res.end();
-        } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
-            const id = req.url.split('/')[3];
-            const user = users.find((user) => user.id === parseInt(id));
-            res.setHeader('Content-Type', 'application/json');
-            if (user) {
-                res.write(JSON.stringify(user));
+        JsonMiddleware(req, res, () => {
+            if (req.url === '/api/users' && req.method === 'GET') {
+                getUsersHandler(req, res);
+            } else if (req.url.match(/\/api\/users\/\d+/) && req.method === 'GET') {
+                getUserByIdHandler(req, res);
             } else {
-                res.statusCode = 404;
-                res.write(JSON.stringify({ message: 'User not found' }));
+                notFoundHandler(req, res);
             }
-            res.end();
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.statusCode = 404;
-            res.write(JSON.stringify({ message: 'Route not found' }));
-            res.end();
-        }
+       });
     });
 });
 
 
+// Iniciar o servidor
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
